85f6bb189a28f26b8b432e9ff74cf3b87d714a0d
brunovicentealves <brunovicentealves.ti@gmail.com>
2021-08-02 21:14:12 -0300

    Project initial

---
+++ src/main/java/com/trabalho/crud/TrabalhofinalcrudApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhofinalcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhofinalcrudApplication.class, args);
+	}
+
+}
