f1bbb1fa34de8d5e501642a4af9daf1ad993e108
brunovicentealves <brunovicentealves.ti@gmail.com>
2021-08-02 21:22:31 -0300

    model client create

---
+++ src/main/java/com/trabalho/crud/entities/Client.java
@@ -0,0 +1,98 @@
+package com.trabalho.crud.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class Client implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	private String name;
+
+	private Double income;
+
+	private Instant birthDate;
+
+	private Integer children;
+
+	public Client(Long id, String name, Double income, Instant birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+	public Client() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else
+			if (!id.equals(other.id))
+				return false;
+		return true;
+	}
+
+}
